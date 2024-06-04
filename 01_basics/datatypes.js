90718d310bf1fc748b55051acd9bdf9dca1645d5
Kunal Garg <116939261+kunalgarg-18@users.noreply.github.com>
2024-06-04 19:34:42 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+ "use strict"; // treat all JS code as newer version
+
+//alert(3+3) //This will open a pop up with 6 in browser but in nodejs it will show error
+
+let name = "Kunal"
+let age = 22
+let isLoggedIn = false
+let state;
+
+//primitive datatypes in JS
+
+//number => 2 ^ 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefinded 
+//symbol
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
