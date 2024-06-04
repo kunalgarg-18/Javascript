78997a1d3b0676766c45f7dd71cee207a15d077d
Kunal Garg <116939261+kunalgarg-18@users.noreply.github.com>
2024-06-04 20:30:24 +0000

    memory discussion

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,4 +32,30 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
+
+
+/* ************Stack and Heap Memory  *********/
+
+//Stack is used for primitive , Heap (Non Primitive)
+
+let myName = "KunalGarg"
+let anotherName = myName
+anotherName = "ChiragGarg"
+
+// console.log(anotherName); //ChiragGarg
+// console.log(myName); //KunalGarg
+
+//This happened because in anotherName, a copy of myName is sent and the change happened in that copy only and not on the original myName
+
+let userOne = {
+    email: "one@google.go",
+    id : "one"
+}
+let userTwo = userOne
+userTwo.email = "two@google.com"
+
+console.log(userOne.email); //two@google.com
+console.log(userTwo.email); //two@google.com
+
+//Both are same because, userOne is an object (Non Primitive Datatype), so it is passed by reference. Both userOne and userTwo are pointing at same object in the heap memory. All the changes are done in original datatype. 
